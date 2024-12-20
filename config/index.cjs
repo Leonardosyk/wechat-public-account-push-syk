1a960f122c018639e95bdaecdd265fe24df8daf6
Leonardosyk <114681503+Leonardosyk@users.noreply.github.com>
2024-12-21 01:36:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,47 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxa23ffdc86028b614',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '60b682f6da6643955b86eb2894819410',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '江苏省',
+  CITY: '苏州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '潇颖宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'owx_m6-0T3NanmceZTw3DU1xCYJI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '🗓️{{date.DATA}} 城市：{{city.DATA}} 天气☁️：{{weather.DATA}} 温度🌡️: {{min_temperature.DATA}}--{{max_temperature.DATA}} 风向：{{wind_direction.DATA}} 风级：{{wind_scale.DATA}} 温馨提示：{{notice.DATA}} 值得纪念：{{birthday_message.DATA}} 💗：{{earthy_love_words.DATA}} 每日一句😋 中文：{{note_ch.DATA}} English：{{note_en.DATA}} 和{{poetry_author.DATA}}约个会：{{poetry_content.DATA}}',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
+      horoscopeDate: '03-25',
+      
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-08-19' },
       ],
     },
   ],
 
-
+  
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
+
+共推送 {{need_post_num.DATA}} 人
+成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
+成功用户: {{success_post_ids.DATA}}
+失败用户: {{fail_post_ids.DATA}}',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: 'syk',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'owx_m6-0T3NanmceZTw3DU1xCYJI',
     }
   ],
 
